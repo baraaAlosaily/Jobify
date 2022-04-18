aa84b2939735524df47b94f73b22cb5b9d4ccfbf
baraaAlosaily <seelbaraa@gmail.com>
2022-04-18 12:31:20 +0300

    complete unitl alert

---
+++ client/src/pages/index.js
@@ -0,0 +1,11 @@
+import DashBoard from "./DashBoard";
+import Error from "./Error";
+import Landing from "./Landing";
+import Register from "./Register";
+
+export {
+    DashBoard,
+    Error,
+    Landing,
+    Register
+}
